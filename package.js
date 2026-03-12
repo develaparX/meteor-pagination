f16453e6723575bbba41988a41a7df8a213d7dd4
arfianssr <arfian.saiful@egogohub.co.id>
2026-03-12 15:06:59 +0700

    chore: update package.js for Meteor 2.x compatibility
    - Add METEOR@2.0 to supported versions
    - Add explicit 'mongo' dependency on client side for Mongo.Collection
    - Maintain backward compatibility with METEOR@1.2.1
    
    Severity: Medium - Compatibility improvement

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 });
 
 Package.onUse((api) => {
-  api.versionsFrom('METEOR@1.2.1');
+  api.versionsFrom(['METEOR@1.2.1', 'METEOR@2.0']);
   api.use([
     'ecmascript',
     'meteor-base',
@@ -16,12 +16,18 @@ Package.onUse((api) => {
     'mongo',
   ]);
 
+  // Mongo.Collection is needed on client for count collection
+  api.use([
+    'mongo',
+  ], 'client');
+
   api.mainModule('server/pagination.js', 'server');
 
   api.use([
     'tracker',
     'reactive-var',
     'reactive-dict',
+    'mongo',
   ], 'client');
 
   api.mainModule('client/pagination.js', 'client');
