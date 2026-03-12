4cbe7904e97fc97b5272b92c026e7d98cea36f17
arfianssr <arfian.saiful@egogohub.co.id>
2026-03-12 15:10:24 +0700

    chore: bump version to 1.2.1
    Version bump for bug fixes and Meteor 2.x compatibility

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'kurounin:pagination',
   summary: 'Meteor pagination done right. Usable in ReactJS or Blaze templates.',
-  version: '1.2.0',
+  version: '1.2.1',
   git: 'https://github.com/Kurounin/Pagination.git',
   documentation: 'README.md',
 });
